ebcb98fa487c4269b7af1b8d915bc56bb431be26
Aditi Ramachandran <aditiramachandran@gmail.com>
2018-01-30 17:44:23 -0500

    testing log file creation with app running for 3 minutes

--- app/src/main/java/tutoringproject/behaviors/Question.java
+++ app/src/main/java/tutoringproject/behaviors/Question.java
@@ -22,7 +22,7 @@ public class Question {
             numerator = question.getString(Questions.KEY_NUMERATOR);
             answerText = question.getString(Questions.KEY_ANSWER);
             spokenQuestion = question.getString(Questions.KEY_SPOKEN_QUESTION);
-            wordProblem = question.getBoolean(Questions.KEY_WORD_PROBLEM); // this string is empty unless the problem is a word problem
+            wordProblem = question.getBoolean(Questions.KEY_WORD_PROBLEM); // this is a boolean indicating whether this problem is a word problem
 
         }catch (Exception e){
             e.printStackTrace();
